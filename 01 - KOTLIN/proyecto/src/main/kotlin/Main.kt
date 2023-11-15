71a79255d7ea5108058631f5c227cb9ea7011514
MAndresT12 <89875869+MAndresT12@users.noreply.github.com>
2023-11-15 07:36:57 -0500

    Add variables and comments to Main.kt

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    //Variables inmutables (val) y mutables (var)
+    val inmutable: String = "Adrian";
+    //inmutable = "Vicente";
+
+    var mutable: String = "Vicente";
+    mutable = "Adrian";
+    
+
 }
