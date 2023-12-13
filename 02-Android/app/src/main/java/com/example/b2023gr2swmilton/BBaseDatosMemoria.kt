53eb5c6522108d5ff1ff253aecf2e7fd65e90633
MAndresT12 <89875869+MAndresT12@users.noreply.github.com>
2023-12-13 16:27:12 -0500

    Add BListView activity and functionality

--- 02-Android/app/src/main/java/com/example/b2023gr2swmilton/BBaseDatosMemoria.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2swmilton/BBaseDatosMemoria.kt
@@ -1,4 +1,21 @@
 package com.example.b2023gr2swmilton
 
 class BBaseDatosMemoria {
+    companion object{
+        val arregloBEntrenador= arrayListOf<BEntrenador>()
+    init {
+        arregloBEntrenador
+            .add(
+                BEntrenador(1,"Adrian","a@a.com")
+            )
+        arregloBEntrenador
+            .add(
+                BEntrenador(2,"Vicente","b@b.com")
+            )
+        arregloBEntrenador
+            .add(
+                BEntrenador(3,"Carolina","c@c.com")
+            )
+    }
+    }
 }
--- 02-Android/app/src/main/java/com/example/b2023gr2swmilton/BEntrenador.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2swmilton/BEntrenador.kt
@@ -1,3 +1,5 @@
+package com.example.b2023gr2swmilton
+
 class BEntrenador (
     var id: Int,
     var nombre: String?,
