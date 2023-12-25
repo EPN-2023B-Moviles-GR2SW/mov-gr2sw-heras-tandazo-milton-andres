9b7a83841801c5b5193f855165021a3aa4b55281
MAndresT12 <89875869+MAndresT12@users.noreply.github.com>
2023-12-25 18:41:47 -0500

    Add serialization imports and handle IO exception in Estudiante class

--- 03 - Deber1/CRUD-DEBER1/src/main/kotlin/entidad/Estudiante.kt
+++ 03 - Deber1/CRUD-DEBER1/src/main/kotlin/entidad/Estudiante.kt
@@ -1,4 +1,10 @@
 package entidad
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import java.io.File
+import java.io.IOException
 
+@Serializable
 class Estudiante {
 }
