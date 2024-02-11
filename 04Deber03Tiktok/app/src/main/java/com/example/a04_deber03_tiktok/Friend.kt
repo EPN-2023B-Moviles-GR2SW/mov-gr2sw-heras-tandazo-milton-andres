32f1a7c27ca74e2d0af16e89424f5492a6379bc9
MAndresT12 <89875869+MAndresT12@users.noreply.github.com>
2024-02-10 22:08:35 -0500

    Create Tiktok Project

---
+++ 04Deber03Tiktok/app/src/main/java/com/example/a04_deber03_tiktok/Friend.kt
@@ -0,0 +1,6 @@
+package com.example.a04_deber03_tiktok
+
+data class Chat (val user:String,
+                 val descripcion: String,
+                 val mensaje: String,
+                 val photo:String)
